c4fa9fc01ba7be5cb0ac874b5e2a84c09071d8f4
Fahim Fardin <fahim.fardin@northsouth.edu>
2026-09-13 23:27:43 +0600

    Initialize React Vite project

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+function App() {
+  return (
+    <div>
+      <h1>Dev Stack Builder</h1>
+    </div>
+  );
+}
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
